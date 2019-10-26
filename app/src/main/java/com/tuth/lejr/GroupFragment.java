8e50666fffba7efe7806dd88bd6a2c55d78b999c
Kevin Liu <kdliu00@gmail.com>
2019-10-26 13:17:13 -0700

    sign in setup

---
+++ app/src/main/java/com/tuth/lejr/GroupFragment.java
@@ -0,0 +1,19 @@
+package com.tuth.lejr;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class GroupFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.group_view, container, false);
+    }
+
+}
