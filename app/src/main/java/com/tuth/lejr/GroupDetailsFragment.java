d94ff22d4323962e1eec47008467f64da1951af4
Kevin Liu <kdliu00@gmail.com>
2019-10-27 01:33:21 -0700

    group details

--- app/src/main/java/com/tuth/lejr/GroupDetailsFragment.java
+++ app/src/main/java/com/tuth/lejr/GroupDetailsFragment.java
@@ -1,6 +1,19 @@
 package com.tuth.lejr;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+
 import androidx.fragment.app.Fragment;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class GroupDetailsFragment extends Fragment {
 
@@ -8,6 +21,21 @@ public class GroupDetailsFragment extends Fragment {
 
     public GroupDetailsFragment(String _groupID) {
         groupID = _groupID;
+    }
+
+    private void setQRCodeImage(String text, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+        byte[] pngData = pngOutputStream.toByteArray();
+
+        Bitmap bmp = BitmapFactory.decodeByteArray(pngData, 0, pngData.length);
+        ImageView image = getView().findViewById(R.id.qr_code);
+
+        image.setImageBitmap(Bitmap.createScaledBitmap(bmp, image.getWidth(),
+                image.getHeight(), false));
     }
 
 }
